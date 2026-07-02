4a6a45ac3f4a450870d59d04582253576c36ea5f
Rajput-Yuvraj <rajputreena9629@gmail.com>
2026-07-02 18:36:36 +0530

    all set up configure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import {app} from "./app.js";
 
-
-dotenv.config({
-    path:'./env'
-})
+dotenv.config();
 
 connectDB()
 .then (()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log('server is running at port : ${process.env.PORT}');
-        
+    const port = process.env.PORT || 8000;
+    app.listen(port,()=>{
+        console.log(`server is running at port : ${port}`);
     })
 })
 .catch((err)=>{
